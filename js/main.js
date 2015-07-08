a4bbf439e61576cdbd8ff4d5ee463ce2adbb4b65
José I. León <jils3000@users.noreply.github.com>
2015-07-08 22:23:01 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -120,11 +120,11 @@ define([
                 
                 GX.params = this.config;
                 if(!GX.params.youtubeId){
-                  GX.params.youtubeId = "IgYzLi8PnLw#t=22";
+                  GX.params.youtubeId = "IgYzLi8PnLw";
                   console.log("Default video");
                 }
                 if(!GX.params.gpxURI){
-                  GX.params.gpxURI = "https://drive.google.com/file/d/0B1q9WHFbtUeJNGIxVDVMRTBWaGs/view?usp=sharing";
+                  GX.params.gpxURI = "https://dl.dropboxusercontent.com/u/186739070/via-verde.gpx";
                   console.log("Default route");
                 }
                 
